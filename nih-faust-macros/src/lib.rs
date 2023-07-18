29b6fd91d841d83f1fd19fa0c050921401f2e42f
obsoleszenz <obsoleszenz@riseup.net>
2023-07-18 18:45:17 +0200

    Remove nih-faust-macros crate

--- nih-faust-macros/src/lib.rs
+++
@@ -1,8 +0,0 @@
-extern crate proc_macro;
-use proc_macro::TokenStream;
-use quote::quote;
-
-#[proc_macro]
-pub fn nih_params_from_faust(_item: TokenStream) -> TokenStream {
-    TokenStream::from(quote! {})
-}
